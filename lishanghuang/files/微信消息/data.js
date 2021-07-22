ab302fe9c6fc8e065fe1a03959e3c6ea7d78a910
Zurdo <1301630456@qq.com>
2021-07-22 21:15:45 +0800

    测试推送

---
+++ lishanghuang/files/微信消息/data.js
@@ -0,0 +1,7 @@
+﻿$axure.loadCurrentPage(
+(function() {
+    var _ = function() { var r={},a=arguments; for(var i=0; i<a.length; i+=2) r[a[i]]=a[i+1]; return r; }
+    var _creator = function() { return _(b,c,d,e,f,_(g,h,i,_(j,k,l,k)),m,[],n,_(h,o),p,[q,r,s,t],u,_(v,w,x,y,g,z,A,_(),B,[],C,_(D,E,F,G,H,_(I,J,K,L),M,null,N,G,G,O,P,Q,null,R,S,T,U,V,W,X,S,Y,Z,_(I,J,K,ba),bb,S,bc,bd,_(be,bf,bg,bh,bi,bh,bj,bh,K,_(bk,bl,bm,bl,bn,bl,bo,bp)),i,_(j,k,l,k)),bq,_(),br,_(),bs,_(bt,[])),bu,_(),bv,_());}; 
+var b="url",c="微信消息.html",d="generationDate",e=new Date(1626959547453.91),f="defaultAdaptiveView",g="name",h="",i="size",j="width",k=0,l="height",m="adaptiveViews",n="sketchKeys",o="s0",p="variables",q="OnLoadVariable",r="menu",s="DayTemp",t="intergal",u="page",v="packageId",w="67d924fe87e74b008c76043320c56990",x="type",y="Axure:Page",z="微信消息",A="notes",B="annotations",C="style",D="baseStyle",E="627587b6038d43cca051c114ac41ad32",F="pageAlignment",G="near",H="fill",I="fillType",J="solid",K="color",L=0xFFFFFFFF,M="image",N="imageAlignment",O="imageRepeat",P="auto",Q="favicon",R="sketchFactor",S="0",T="colorStyle",U="appliedColor",V="fontName",W="Applied Font",X="borderWidth",Y="borderVisibility",Z="borderFill",ba=0xFF797979,bb="cornerRadius",bc="cornerVisibility",bd="outerShadow",be="on",bf=false,bg="offsetX",bh=5,bi="offsetY",bj="blurRadius",bk="r",bl=0,bm="g",bn="b",bo="a",bp=0.349019607843137,bq="adaptiveStyles",br="interactionMap",bs="diagram",bt="objects",bu="masters",bv="objectPaths";
+return _creator();
+})());
